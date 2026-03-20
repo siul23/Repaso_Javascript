6226aa281aef73288f6a098f0848dc10db665620
siul23 <luis7arquez@gmail.com>
2026-03-20 15:32:56 -0500

    Crea objetos literales con propiedades y métodos

---
+++ JavaScript/POO/POO-Objects.js
@@ -0,0 +1,14 @@
+
+const Auto = {
+    marca: 'Toyota',
+    modelo: 'Supra MK4',
+    anio: '2002',
+    color: 'Rojo',
+    tipo: 'coupé'
+}
+document.write('Creacion POO - Auto', '<br>')
+document.write('Marca: ', Auto.marca, '<br>')
+document.write('Modelo: ', Auto.modelo, '<br>')
+document.write('Año: ', Auto.anio, '<br>')
+document.write('Color: ', Auto.color, '<br>')
+document.write('Tipo: ', Auto.tipo, '<br>')
